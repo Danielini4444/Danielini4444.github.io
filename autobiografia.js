af1c39886b3e12f3640ff96b605bd0dc24849e29
Danielini4444 <l22240014@smartin.tecnm.mx>
2025-02-28 01:21:45 -0600

    Add files via upload

---
+++ autobiografia.js
@@ -0,0 +1,51 @@
+function nombre() {
+	alert('Daniel Díaz Rojas')
+}
+function grupo() {
+	alert('6A ISC')
+}
+function artifav() {
+	alert('NSQK\n' +
+		'Damiano David\n' +
+		'Two feet\n' +
+		'DPR IAN')
+}
+function persoamada() {
+	alert('Mi chefsita <3, estoy tan agradecida de haberla conocido, la adoro demasiado')
+}
+function lover() {
+	alert('Carolina Pinto Negrete')
+}
+function edad() {
+	alert('21 añitos sin bañarme JAJAJAJA')
+}
+function datocurioso() {
+	alert('Pienso en con quién estás cuando no estás conmigo\n' +
+		'Odio que mis planes no sean tu camino\n' +
+		'Y haberlo negado por estar contigo\n' +
+		'¿Y quién me va a curar el corazón partío?')
+}
+function fotouno(){
+	alert('Esta foto es de mis tres años, aunque suene raro, fui catolico mitad de mi vida')
+}
+function fotodos(){
+	alert('Esta foto es de mis favoritas, fue cuando tenia poco tiempo de empezar en el TKD')
+}
+function fototres(){
+	alert('Esta foto es sin duda mi favorita de todas, la historia detras es linda, y la foto es sencillamente perfecta')
+}
+function fotocuatro(){
+	alert('Esta foto siento que representa mis vibes actuales y mi forma de vestir actualmente')
+}
+function fotocinco(){
+	alert('Esta fue mi etapa de reencontrame conmigo mismo hasta que volvi a ser yo')
+}
+function instagram(){
+	 window.location.href = "https://www.instagram.com/danieldiaz.tkd/";
+}
+function facebook(){
+	 window.location.href = "https://www.facebook.com/adsafaga.a";
+}
+function horario(){
+     window.location.href = "Horario/horario.html";
+}
